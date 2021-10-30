e97a15d3c4e75a38ef759fcd25ffc1c1f5175d20
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-10-30 18:25:46 -0400

    Implemented getInterviewersForDay function for Adding an Intervierwers Selector activity

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,6 +13,19 @@ export function getAppointmentsForDay(state, day) {
   return currentDayAppoinments;
 }
 
+export function getInterviewersForDay(state, day) {
+  if (state.days.length < 1) return [];
+  
+  const filteredDay = state.days.find((d) => d.name === day);
+  if (!filteredDay) return [];
+
+  const currentDayInterviewers = [];
+  for (const elem of filteredDay.interviewers) {
+    currentDayInterviewers.push(state.interviewers[elem]);
+  }
+  return currentDayInterviewers;
+}
+
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
